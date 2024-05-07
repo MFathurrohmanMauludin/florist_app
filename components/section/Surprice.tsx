928c88d05212ffc56b50c532392cf240e713b5cb
M Fathurrohman Mauludin <fathurdiakfari97@gmail.com>
2024-05-07 21:22:36 +0700

    update

--- components/section/Surprice.tsx
+++ components/section/Surprice.tsx
@@ -1,37 +1,48 @@
-import { Image } from '@nextui-org/react'
-import React from 'react'
+import Image from 'next/image'
+
 
 export default function Surprice() {
     const data = [{
         title: 'wedding',
-        image: ['https://i.ibb.co/hVF9Mg6/wedding.jpg', 'https://i.ibb.co/87Bf91G/wedding-2.jpg', 'https://i.ibb.co/87Bf91G/wedding-3.jpg', 'https://i.ibb.co/87Bf91G/wedding-4.jpg', 'https://i.ibb.co/87Bf91G/wedding-5.jpg', 'https://i.ibb.co/87Bf91G/wedding-6.jpg', 'https://i.ibb.co/87Bf91G/wedding-7.jpg'],
-        slogan: 'Give the best and most beautiful flower bouquet to your beloved'
+        image: [
+            "https://i.ibb.co/hVF9Mg6/wedding.jpg",
+            "https://i.ibb.co/87Bf91G/wedding-2.jpg",
+            "https://i.ibb.co/F5bGnwG/wedding-3.jpg",
+            "https://i.ibb.co/hmqqT4G/wedding-4.jpg",
+            "https://i.ibb.co/Y8JWypW/wedding-5.jpg",
+            "https://i.ibb.co/gwkMPDS/wedding-6.jpg",
+            "https://i.ibb.co/W3MnDNm/wedding-7.jpg",
+        ],
+        slogan: 'Give the best and most beautiful flower bouquet to your beloved',
     }]
 
     return (
         <>
             <div className="grid grid-cols-2 px-8 py-[100px] gap-4">
                 {/* slogan */}
-                <div className="flex flex-col items-center justify-center text-center">
+                <div className="flex flex-col relative items-center justify-center text-center">
                     <span
                         className="flex items-center line-color"
                     >
                         Wedding
                     </span>
                     <span className="text-4xl font-semibold capitalize tracking-wide w-[calc(100%_-_50px)]">Give the best and most beautiful flower bouquet to your beloved</span>
+                    <Image className="absolute right-0 top-0 -z-[1]" src="https://i.ibb.co/6vnRzBS/cherry-blossom.png" width={300} height={100} alt='cherry-blossom' />
                 </div>
 
                 {/* image */}
                 <div className="flex flex-row justify-center">
-                    {/* <Image src='https://i.ibb.co/n8vRvkY/wedding.jpg' width={500} alt='give precent' /> */}
+                    {/* <Image src='https://i.ibb.co/n8vRvkY/wedding.jpg' width={500} height={100} alt='give precent' /> */}
                     <div className="columns-3">
-                        <Image className="mb-3 w-full max-w-[216px]" src="https://source.unsplash.com/random/1" width={400} alt="random-1" />
-                        <Image className="mb-3 w-full max-w-[216px]" src="https://source.unsplash.com/random/2" width={400} alt="random-2" />
-                        <Image className="mb-3 w-full max-w-[216px]" src="https://source.unsplash.com/random/3" width={400} alt="random-3" />
-                        <Image className="mb-3 w-full max-w-[216px]" src="https://source.unsplash.com/random/4" width={400} alt="random-4" />
-                        <Image className="mb-3 w-full max-w-[216px]" src="https://source.unsplash.com/random/5" width={400} alt="random-5" />
-                        <Image className="mb-3 w-full max-w-[216px]" src="https://source.unsplash.com/random/6" width={400} alt="random-6" />
-                        <Image className="mb-3 w-full max-w-[216px]" src="https://source.unsplash.com/random/7" width={400} alt="random-7" />
+                        {/* <div className="inline-block box-border overflow-hidden mb-3 w-full max-w-[216px] rounded-[14px]">
+                        </div> */}
+                        <Image className="mb-3 w-full max-w-[216px] rounded-[14px]" src="https://i.ibb.co/hVF9Mg6/wedding.jpg" width={400} height={100} alt="random-1" />
+                        <Image className="mb-3 w-full max-w-[216px] rounded-[14px]" src="https://i.ibb.co/87Bf91G/wedding-2.jpg" width={400} height={100} alt="random-2" />
+                        <Image className="mb-3 w-full max-w-[216px] rounded-[14px]" src="https://i.ibb.co/F5bGnwG/wedding-3.jpg" width={400} height={100} alt="random-3" />
+                        <Image className="mb-3 w-full max-w-[216px] rounded-[14px]" src="https://i.ibb.co/hmqqT4G/wedding-4.jpg" width={400} height={100} alt="random-4" />
+                        <Image className="mb-3 w-full max-w-[216px] rounded-[14px]" src="https://i.ibb.co/Y8JWypW/wedding-5.jpg" width={400} height={100} alt="random-5" />
+                        <Image className="mb-3 w-full max-w-[216px] rounded-[14px]" src="https://i.ibb.co/gwkMPDS/wedding-6.jpg" width={400} height={100} alt="random-6" />
+                        <Image className="mb-3 w-full max-w-[216px] rounded-[14px]" src="https://i.ibb.co/W3MnDNm/wedding-7.jpg" width={400} height={100} alt="random-7" />
                     </div>
                 </div>
             </div>
