a363162854cb17c1a1e769710d42f3ec35cce9ce
M Fathurrohman Mauludin <fathurdiakfari97@gmail.com>
2024-05-08 22:35:04 +0700

    update

--- components/section/Surprice.tsx
+++ components/section/Surprice.tsx
@@ -2,19 +2,36 @@ import Image from 'next/image'
 
 
 export default function Surprice() {
-    const data = [{
-        title: 'wedding',
-        image: [
-            "https://i.ibb.co/hVF9Mg6/wedding.jpg",
-            "https://i.ibb.co/87Bf91G/wedding-2.jpg",
-            "https://i.ibb.co/F5bGnwG/wedding-3.jpg",
-            "https://i.ibb.co/hmqqT4G/wedding-4.jpg",
-            "https://i.ibb.co/Y8JWypW/wedding-5.jpg",
-            "https://i.ibb.co/gwkMPDS/wedding-6.jpg",
-            "https://i.ibb.co/W3MnDNm/wedding-7.jpg",
-        ],
-        slogan: 'Give the best and most beautiful flower bouquet to your beloved',
-    }]
+    const data = [
+        {
+            title: 'wedding',
+            image: [
+                "https://i.ibb.co/hVF9Mg6/wedding.jpg",
+                "https://i.ibb.co/87Bf91G/wedding-2.jpg",
+                "https://i.ibb.co/F5bGnwG/wedding-3.jpg",
+                "https://i.ibb.co/hmqqT4G/wedding-4.jpg",
+                "https://i.ibb.co/Y8JWypW/wedding-5.jpg",
+                "https://i.ibb.co/gwkMPDS/wedding-6.jpg",
+                "https://i.ibb.co/W3MnDNm/wedding-7.jpg",
+            ],
+            slogan: 'Give the best and most beautiful flower bouquet to your beloved',
+            url: '',
+        },
+        {
+            title: 'graduation',
+            image: [
+                "https://img.freepik.com/premium-photo/women-is-graduate-holding-bouquet-smiling-camera_7192-703.jpg?w=720",
+                "https://img.freepik.com/free-photo/back-view-elegant-woman-holding-bouquet-flowers-outside_23-2148826696.jpg?t=st=1715181937~exp=1715185537~hmac=eb264483378300988e36994ab5c0b69d929656b59915d846221ff9c83a59b261&w=740",
+                "https://img.freepik.com/premium-photo/portrait-smiling-student-wearing-graduation-gown-while-holding-bouquet-outdoors_1048944-30330747.jpg?w=740",
+                "https://img.freepik.com/premium-photo/portrait-woman-graduation-gown_1048944-20155993.jpg?w=500",
+                "https://img.freepik.com/premium-photo/girl-holding-bouquet_29207-167.jpg?w=740",
+                "https://img.freepik.com/premium-photo/businessman-hold-hiding-flower-bouquet-congratulation-young-woman-bachelor-s-degree-graduated_49071-4912.jpg?w=740",
+            ],
+            slogan: 'Give the best and most beautiful flower bouquet to your beloved',
+            url: '',
+        },
+
+    ]
 
     return (
         <>
