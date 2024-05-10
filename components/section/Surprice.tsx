4ff9fdf69498a0fdb31c03e985368eacec0827f5
M Fathurrohman Mauludin <fathurdiakfari97@gmail.com>
2024-05-10 17:24:42 +0700

    update

--- components/section/Surprice.tsx
+++ components/section/Surprice.tsx
@@ -1,3 +1,6 @@
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { Button, Link } from '@nextui-org/react'
 import Image from 'next/image'
 
 
@@ -28,7 +31,7 @@ export default function Surprice() {
                 "https://img.freepik.com/premium-photo/businessman-hold-hiding-flower-bouquet-congratulation-young-woman-bachelor-s-degree-graduated_49071-4912.jpg?w=740",
                 "https://img.freepik.com/premium-photo/rear-view-photographer-photographing-woman-graduation-gown-against-trees_1048944-26805735.jpg?w=740",
             ],
-            slogan: 'mari sambut orang terdekat kita dengan keindahan bunga',
+            slogan: 'mari sambut kebahagian dengan keindahan bunga',
             url: '',
         },
 
@@ -38,15 +41,42 @@ export default function Surprice() {
         <>
             <div className="grid grid-cols-2 px-8 py-[100px] gap-4">
                 {/* slogan */}
-                <div className="flex flex-col relative items-center text-center">
-                    <span
-                        className="flex items-center text-2xl line-color mt-[50px]"
-                    >
-                        Wedding
-                    </span>
+                <div className="flex flex-col relative items-center justify-center text-center">
+                    <div className='flex flex-row items-center justify-center gap-x-2 w-full'>
+                        <div className='flex grow h-[3px] w-full max-w-[160px] bg-pink-500 rounded' />
+                        <span
+                            className="flex items-center text-2xl"
+                        >
+                            Wedding
+                        </span>
+                        <div className='flex grow h-[3px] w-full max-w-[160px] bg-pink-500 rounded' />
+                    </div>
                     <span className="text-4xl font-semibold capitalize tracking-wide w-[calc(100%_-_50px)]">Give the best and most beautiful flower bouquet to your beloved</span>
+
+                    <div className="flex gap-x-3 pt-6">
+                        <Button
+                            as={Link}
+                            className='!bg-pink-600 hover:!bg-pink-500 capitalize w-full max-w-[300px]'
+                            href='#'
+                            variant='solid'
+                            color='primary'
+                            radius='full'
+                        >
+                            check now
+                        </Button>
+
+                        <Button
+                            as={Link}
+                            className='text-pink-600 hover:!bg-pink-700 hover:!text-white capitalize w-full max-w-[300px]'
+                            href='#'
+                            variant='light'
+                            color='primary'
+                            radius='full'
+                        >
+                            see more
+                        </Button>
+                    </div>
                     {/* <Image className="absolute right-0 top-0 -z-[1]" src="https://i.ibb.co/6vnRzBS/cherry-blossom.png" width={256} height={100} alt='cherry-blossom' /> */}
-                    <Image className="absolute top-[33%] -z-[1]" src="https://i.ibb.co/2W5XcdN/couple-with-lovely-wedding-no-bg.png" width={400} height={100} alt='cherry-blossom' />
                 </div>
 
                 {/* image */}
